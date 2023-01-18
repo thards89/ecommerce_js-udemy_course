b3ed68d86480793b34c7a2901466af37888e9505
thards89 <thards89@gmail.com>
2023-01-18 03:36:27 +0100

    image upload logic inserted

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const { validationResult } = require('express-validator')
+const multer = require('multer');
+
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/products/new");
 const { requireTitle, requirePrice } = require("./validators")
 
+
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage()})
 
 router.get("/admin/products", (req, res) => {});
 
@@ -12,11 +16,18 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [
-    requireTitle, requirePrice
-], (req, res) => {
+router.post('/admin/products/new', upload.single('image'), [
+   requireTitle, requirePrice, 
+], async (req, res) => {
     const errors = validationResult(req)
-    console.log(errors)
+
+    if (!errors.isEmpty()) {
+        return res.send(productsNewTemplate({ errors }))
+    }
+
+    const image = req.file.buffer.toString('base64')
+    const { title, price } = req.body
+    await productsRepo.create({ title, price, image })
 
     res.send('Submited')
 }
